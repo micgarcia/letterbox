f67f75de95e5fe7618e0638f4d913290805f7e92
micgarcia <micgarcia9185@gmail.com>
2022-09-06 20:52:19 -0700

    Started basic styling of homepage (background color, images, layout)

--- letterbox/src/components/Home.js
+++ letterbox/src/components/Home.js
@@ -6,16 +6,17 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   const [backdropUrl, setBackdropUrl] = useState('');
   const [trendingUrl, setTrendingUrl] = useState([]);
-  useEffect(() => {
-    /* fetches backdrop image for Nope */
-    fetch('https://api.themoviedb.org/3/movie/762504?api_key=5ea30c3df8f6f36a3bae33585f1396c7&language=en-US', {mode: 'cors'})
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(response) {
-        setBackdropUrl('https://image.tmdb.org/t/p/original' + response.backdrop_path)
-    });
 
+  /* fetches backdrop image for Nope */
+  fetch('https://api.themoviedb.org/3/movie/762504?api_key=5ea30c3df8f6f36a3bae33585f1396c7&language=en-US', {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    setBackdropUrl('https://image.tmdb.org/t/p/original' + response.backdrop_path)
+  });
+
+  useEffect(() => {
     /* Supposed to fetch poster urls for trending movies */
     fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=5ea30c3df8f6f36a3bae33585f1396c7', {mode: 'cors'})
       .then(function(response) {
@@ -24,34 +25,34 @@ const Home = () => {
       .then(function(response) {
         console.log(response);
         for (let i = 0; i < 20; i++) {
-          if (response.results[i].popularity < 2500) {
+          if (response.results[i].popularity < 3000) {
             setTrendingUrl((prevUrls) => [...prevUrls, ('https://image.tmdb.org/t/p/original' + response.results[i].poster_path)]);
           }
         }
-        console.log(trendingUrl);
       })
   }, [])
 
+  console.log(trendingUrl);
 
   return (
     <div id='homePage'>
-      <div className="navBar">
-        {<Navbar />}
-      </div>
+      {<Navbar />}
       <div className="homeContent">
         <div className="picCont">
-          <img src={backdropUrl} alt="" />
         </div>
         <div className="siteDesc">
           Track films you've watched. <br/>
           Save those you want to see.
         </div>
         <div className="trendingCont">
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <img src={trendingUrl[0]} alt="" />
+          <img src={trendingUrl[1]} alt="" />
+          <img src={trendingUrl[2]} alt="" />
+          <img src={trendingUrl[3]} alt="" />
+          <img src={trendingUrl[4]} alt="" />
+          <img src={trendingUrl[5]} alt="" />
+          <img src={trendingUrl[6]} alt="" />
+          <img src={trendingUrl[7]} alt="" />
         </div>
       </div>
     </div>
--- letterbox/src/index.js
+++ letterbox/src/index.js
@@ -6,8 +6,6 @@ import app from "./components/utils/firebase.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
