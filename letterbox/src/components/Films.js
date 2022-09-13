794af330b158c0814d9bf3c78d33384031f06c39
micgarcia <micgarcia9185@gmail.com>
2022-09-12 21:10:41 -0700

    Fixed bugs with filter system and added genre filtering, and 2 pages worth of results

--- letterbox/src/components/Films.js
+++ letterbox/src/components/Films.js
@@ -19,7 +19,6 @@ const Films = () => {
       })
       .then(function(response) {
         for (let i = 0; i < 20; i++) {
-          console.log(response);
           setCurrentIDs((prevIDs) => [...prevIDs, response.results[i].id]);
           setCurrentPics((prevPics) => [...prevPics, ('https://image.tmdb.org/t/p/original' + response.results[i].poster_path)])
         }
@@ -27,43 +26,75 @@ const Films = () => {
   },[])
 
   const sortPop = () => {
-    const grid = document.querySelector('.resultsGrid');
-    for (let child of grid.children) {
-      child.remove();
-    }
+    setCurrentIDs([]);
+    setCurrentPics([]);
 
-    fetch('https://api.themoviedb.org/3/discover/movie?api_key=5ea30c3df8f6f36a3bae33585f1396c7&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=53&with_watch_monetization_types=flatrate', {mode: 'cors'})
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=5ea30c3df8f6f36a3bae33585f1396c7&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1', {mode: 'cors'})
       .then(function(response) {
         return response.json();
       })
       .then(function(response) {
         for (let i = 0; i < 20; i++) {
-          console.log(response);
           setCurrentIDs((prevIDs) => [...prevIDs, response.results[i].id]);
-          setCurrentPics((prevPics) => [...prevPics, ('https://image.tmdb.org/t/p/original' + response.results[i].poster_path)])
+          setCurrentPics((prevPics) => [...prevPics, ('https://image.tmdb.org/t/p/original' + response.results[i].poster_path)]);
         }
+        fetch('https://api.themoviedb.org/3/discover/movie?api_key=5ea30c3df8f6f36a3bae33585f1396c7&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2', {mode: 'cors'})
+          .then(function(json) {
+            return json.json();
+          })
+          .then(function(data) {
+            for (let i = 0; i < 20; i++) {
+              setCurrentIDs((prevIDs) => [...prevIDs, data.results[i].id]);
+              setCurrentPics((prevPics) => [...prevPics, ('https://image.tmdb.org/t/p/original' + data.results[i].poster_path)]);
+            }
+          })
       })
   }
 
   const sortRecent = () => {
-    const grid = document.querySelector('.resultsGrid');
-    for (let child of grid.children) {
-      child.remove();
-    }
+    setCurrentIDs([]);
+    setCurrentPics([]);
 
-    fetch('https://api.themoviedb.org/3/discover/movie?api_key=5ea30c3df8f6f36a3bae33585f1396c7&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate', {mode: 'cors'})
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=5ea30c3df8f6f36a3bae33585f1396c7&language=en&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&year=2022&vote_average.gte=6', {mode: 'cors'})
       .then(function(response) {
         return response.json();
       })
       .then(function(response) {
         for (let i = 0; i < 20; i++) {
-          console.log(response);
           setCurrentIDs((prevIDs) => [...prevIDs, response.results[i].id]);
-          setCurrentPics((prevPics) => [...prevPics, ('https://image.tmdb.org/t/p/original' + response.results[i].poster_path)])
+          setCurrentPics((prevPics) => [...prevPics, ('https://image.tmdb.org/t/p/original' + response.results[i].poster_path)]);
         }
+
       })
+  }
 
+  const sortGenre = () => {
+    let selectElement = document.querySelector('#genres');
+    let genre = selectElement.options[selectElement.selectedIndex].value;
+    setCurrentIDs([]);
+    setCurrentPics([]);
 
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=5ea30c3df8f6f36a3bae33585f1396c7&language=en&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_average.gte=6&with_genres=' + genre, {mode: 'cors'})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      console.log(response);
+      for (let i = 0; i < 20; i++) {
+        setCurrentIDs((prevIDs) => [...prevIDs, response.results[i].id]);
+        setCurrentPics((prevPics) => [...prevPics, ('https://image.tmdb.org/t/p/original' + response.results[i].poster_path)]);
+      }
+      fetch('https://api.themoviedb.org/3/discover/movie?api_key=5ea30c3df8f6f36a3bae33585f1396c7&language=en&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&vote_average.gte=6&with_genres=' + genre, {mode: 'cors'})
+          .then(function(json) {
+            return json.json();
+          })
+          .then(function(data) {
+            for (let i = 0; i < 20; i++) {
+              setCurrentIDs((prevIDs) => [...prevIDs, data.results[i].id]);
+              setCurrentPics((prevPics) => [...prevPics, ('https://image.tmdb.org/t/p/original' + data.results[i].poster_path)]);
+            }
+          })
+    })
   }
 
   //Change genre values to genre codes
@@ -73,25 +104,25 @@ const Films = () => {
       <div className="filmsContent">
         <div className="filters">
           <label htmlFor="genres">Genres</label>
-          <select name="genres" id="genres">
-            <option value="Action">Action</option>
-            <option value="Adventure">Adventure</option>
-            <option value="Animation">Animation</option>
-            <option value="Comedy">Comedy</option>
-            <option value="Crime">Crime</option>
-            <option value="Documentary">Documentary</option>
-            <option value="Drama">Drama</option>
-            <option value="Family">Family</option>
-            <option value="Fantasy">Fantasy</option>
-            <option value="History">History</option>
-            <option value="Horror">Horror</option>
-            <option value="Music">Music</option>
-            <option value="Mystery">Mystery</option>
-            <option value="Romance">Romance</option>
-            <option value="Science Fiction">Science Fiction</option>
-            <option value="Thriller">Thriller</option>
-            <option value="War">War</option>
-            <option value="Western">Western</option>
+          <select name="genres" id="genres" onChange={sortGenre}>
+            <option value="28">Action</option>
+            <option value="12">Adventure</option>
+            <option value="16">Animation</option>
+            <option value="35">Comedy</option>
+            <option value="80">Crime</option>
+            <option value="99">Documentary</option>
+            <option value="18">Drama</option>
+            <option value="10751">Family</option>
+            <option value="14">Fantasy</option>
+            <option value="36">History</option>
+            <option value="27">Horror</option>
+            <option value="10402">Music</option>
+            <option value="9648">Mystery</option>
+            <option value="10749">Romance</option>
+            <option value="878">Science Fiction</option>
+            <option value="53">Thriller</option>
+            <option value="10752">War</option>
+            <option value="37">Western</option>
           </select>
 
           <label htmlFor="year" onClick={sortRecent}>Sort By Recent</label>
