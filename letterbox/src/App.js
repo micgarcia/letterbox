60889404f27d15117111394049f00de13b7361b0
micgarcia <micgarcia9185@gmail.com>
2022-09-16 13:39:37 -0700

    Refactored router and index.html to publish on github pages

--- letterbox/src/App.js
+++ letterbox/src/App.js
@@ -1,8 +1,9 @@
 import './Home.css';
 import {
   BrowserRouter,
+  HashRouter,
   Routes,
-  Route,
+  Route
 } from "react-router-dom";
 import Home from './components/Home.js';
 import Films from './components/Films.js';
@@ -19,17 +20,17 @@ function App() {
   return (
 
     <div className="App">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/films' element={<Films />}></Route>
-        <Route path='/info' element={<Info />}></Route>
-        <Route path='/create' element={<Create />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/account' element={<Account />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <HashRouter>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/films' element={<Films />}></Route>
+            <Route path='/info' element={<Info />}></Route>
+            <Route path='/create' element={<Create />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/account' element={<Account />}></Route>
+          </Routes>
+        </HashRouter>
     </div>
   );
 }
--- letterbox/src/index.js
+++ letterbox/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import app from "./components/utils/firebase.js";
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
+
 );
 
